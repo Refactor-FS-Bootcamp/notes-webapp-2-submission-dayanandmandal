d74cf58dcfffbfd1685e1f089fdb1b7ae4a194e9
dayanandmandal <120941312+dayanandmandal@users.noreply.github.com>
2023-02-17 11:45:55 +0530

    Update global-onclick-event.js

--- js/global-onclick-event.js
+++ js/global-onclick-event.js
@@ -1,19 +1,19 @@
-function globalClickEvent(event) {
-  const newNote = document.querySelector(".header-new-notes");
-  if (newNote.contains(event.target)) {
-    return;
-  }
+// function globalClickEvent(event) {
+//   const newNote = document.querySelector(".header-new-notes");
+//   if (newNote.contains(event.target)) {
+//     return;
+//   }
 
-  const formNote = document.querySelector(".main-form-add-notes");
-  if (formNote.contains(event.target)) {
-    return;
-  }
+//   const formNote = document.querySelector(".main-form-add-notes");
+//   if (formNote.contains(event.target)) {
+//     return;
+//   }
 
-  const divShowHide = document.querySelector(".header-new-notes");
-  if (divShowHide.dataset.formDisplay == "show") {
-    divShowHide.dataset.formDisplay = "hide";
-    hideForm();
-    allowScrolling();
-    unBlurBackground();
-  }
-}
+//   const divShowHide = document.querySelector(".header-new-notes");
+//   if (divShowHide.dataset.formDisplay == "show") {
+//     divShowHide.dataset.formDisplay = "hide";
+//     hideForm();
+//     allowScrolling();
+//     unBlurBackground();
+//   }
+// }
